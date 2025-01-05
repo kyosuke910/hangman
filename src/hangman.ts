a398a96793c7cc77c0dd8a44ea031c29bfef107c
*SJrGDXtfAq <ochiai@digimake.co.jp>
2025-01-05 14:36:04 +0900

    出題問題の型の定義、JSONから読み込んだデータの代入

--- src/hangman.ts
+++ src/hangman.ts
@@ -1,2 +1,8 @@
-console.log("Game Start!!")
+import rawData from "./data/questions.test.json"
+interface Question {
+  word: string
+  hint: string
+}
 
+// Question[]型を指定
+const question: Question[] = rawData // JSONから読み込んだデータを代入
